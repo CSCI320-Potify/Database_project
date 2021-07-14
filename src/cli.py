9abed7576c37b952c0b97e5f74e3f04012468cb5
Raymond Hu <rh8677@g.rit.edu>
2021-07-13 22:13:40 -0400

    Implemented the add collection function

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,85 @@
 from src.db import *
 import datetime
 
+
+def add_to_collection():
+    connection = connect()
+    cursor = connection.cursor()
+    while True:
+        collect = input("What is the name of the collection you wish to add to?\n")
+        if collect == "quit":
+            break
+        cursor.execute('SELECT COUNT(name) FROM "collection" WHERE name=%s', ([collect]))
+        find_col = cursor.fetchone()[0]
+        if find_col == 1:
+            song = input("What is the name of the song you wish to add to " + collect + "?\n")
+            if song == "quit":
+                break
+            cursor.execute('SELECT COUNT(Title) FROM "Song" WHERE Title=%s', ([song]))
+            find_song = cursor.fetchone()[0]
+            if find_song == 1:
+                cursor.execute('INSERT INTO "collection-song"(Collection_num, song_num) VALUES (1, 2)')
+                connection.commit()
+            else:
+                print("The specified song was not found")
+        else:
+            print("The specified collection was not found")
+    connection.close()
+
+
+def create_collection(user):
+    connection = connect()
+    cursor = connection.cursor()
+    collect = input("What is the name of the collection you wish to add?\n")
+    if collect == "quit":
+        return
+    cursor.execute('SELECT COUNT(*) FROM "collection" WHERE name=%s AND username=%s', (collect, user))
+    exists = cursor.fetchone()[0]
+    if exists == 0:
+        cursor.execute('SELECT MAX(collection_num) FROM "collection"')
+        new_num = cursor.fetchone()[0] + 1
+        cursor.execute('INSERT INTO "collection" VALUES (%s, %s, %s, %s, %s)', (collect, new_num, "0", "0", user))
+        connection.commit()
+        print("Collection successfully added!")
+    else:
+        print("A collection with this name already exists!")
+    connection.close()
+
+
+def collections(user):
+    while True:
+        print("Hello " + user)
+        print("Select one of the following options")
+        print("1: Add to collection")
+        print("2: Create new collection")
+        print("3: Delete from collection")
+        print("4: Delete a collection")
+        print("5: Play a collection")
+        print("6: Rename a collection")
+        print("7: View all collections")
+        print("8: Go back")
+        choice = input()
+
+        if choice == "1":
+            add_to_collection(user)
+        elif choice == "2":
+            create_collection(user)
+        elif choice == "3":
+            delete_from_collection(user)
+        elif choice == "4":
+            delete_collection(user)
+        elif choice == "5":
+            play_collection()
+        elif choice == "6":
+            rename_collection()
+        elif choice == "7":
+            view_collections()
+        elif choice == "8":
+            break
+        else:
+            print("Unknown command")
+
+
 def signup():
     first_name = input("Enter First Name:\n")
     last_name = input("Enter Last Name:\n")
